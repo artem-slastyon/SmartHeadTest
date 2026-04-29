062154495576417130b93ed089c787fb7acc1f05
HACKERPRO17 <sartyom532@gmail.com>
2026-04-29 13:51:33 +0300

    feat: Created widget page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\TicketsController;
+use App\Http\Controllers\WidgetController;
 use Illuminate\Support\Facades\Route;
 
 Auth::routes();
@@ -8,3 +9,5 @@ Auth::routes();
 Route::redirect('/', '/tickets');
 
 Route::resource('/tickets', TicketsController::class);
+
+Route::get('/widget', [WidgetController::class, 'index']);
