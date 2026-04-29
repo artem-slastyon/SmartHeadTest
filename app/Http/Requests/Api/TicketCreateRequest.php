ad07ccca78ccdaf6a7bde0731d2b292ab2ba6f12
Artem S. <artyom.slastyon@gmail.com>
2026-04-29 21:10:13 +0300

    feat: Widget implementation

--- app/Http/Requests/Api/TicketCreateRequest.php
+++ app/Http/Requests/Api/TicketCreateRequest.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Requests\Api;
 
+use App\Support\SizeConverter;
+use Exception;
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
@@ -11,16 +13,19 @@ class TicketCreateRequest extends FormRequest
      * Get the validation rules that apply to the request.
      *
      * @return array<string, ValidationRule|array|string>
+     * @throws Exception
      */
     public function rules(): array
     {
+        $maxSize = SizeConverter::toKilobytes(config('validation.maxFileSize'));
+
         return [
-            'name' => 'string',
-            'email' => 'email',
+            'name' => 'string|required',
+            'email' => 'email|required',
             'phone' => 'nullable|regex:' . config('validation.phoneRegex'),
-            'subject' => 'string',
-            'text' => 'string',
-            'attachments.*' => 'file|max:5'
+            'subject' => 'string|required',
+            'text' => 'string|required',
+            'attachments.*' => 'file|max:' . config('validation.maxFileCount') . '|size:' . $maxSize,
         ];
     }
 }
--- resources/js/widget.js
+++ resources/js/widget.js
@@ -1,82 +1,87 @@
+import { getErrorElement, getErrorMessage, getFieldNameById } from './utils/form-validation.js';
+import { convertFileSizeToBytes } from './utils/datasize.js';
+
 function validateForm () {
-    /**
-     *
-     * @param {HTMLInputElement} input
-     * @returns {Element}
-     */
-    const getErrorElement = function (input) {
-        let el = input.nextElementSibling
-        if (el === null || el.tagName.toLowerCase() !== 'small') {
-            el = document.createElement('small')
-            el.classList.add('required')
-            input.after(el)
-        }
-
-        return el
-    }
-
-    /**
-     *
-     * @param {string} id
-     * @returns {string}
-     */
-    const getFieldNameById = function (id) {
-        const string = id.replaceAll('-', ' ')
-        return string.charAt(0).toUpperCase() + string.substring(1)
-    }
-
-    /**
-     *
-     * @param {HTMLInputElement} el
-     * @return {string}
-     */
-    const getErrorMessage = function (el) {
-        if (el.validity.tooLong) {
-            return 'too long!'
-        }
-
-        if (el.validity.tooShort) {
-            return 'too short!'
-        }
-
-        if (el.validity.valueMissing) {
-            return 'is required!'
-        }
-
-        return 'has invalid format!'
-    }
-
-    let formValid = true
+    let formValid = true;
 
     const validator = function (inputElement) {
-        const errorElement = getErrorElement(inputElement)
+        const errorElement = getErrorElement(inputElement);
 
         if (inputElement.checkValidity()) {
-            errorElement.hidden = true
+            errorElement.hidden = true;
         } else {
-            const fieldName = getFieldNameById(inputElement.id)
-            const errorMessage = getErrorMessage(inputElement)
+            const fieldName = getFieldNameById(inputElement.id);
+            const errorMessage = getErrorMessage(inputElement);
 
-            errorElement.textContent = `${fieldName} ${errorMessage}`
-            errorElement.hidden = false
+            errorElement.textContent = `${fieldName} ${errorMessage}`;
+            errorElement.hidden = false;
 
-            formValid = false
+            formValid = false;
         }
     }
 
     document.querySelectorAll('input, textarea').forEach(validator);
 
-    return formValid
+    return formValid;
+}
+
+/**
+ *
+ * @param el
+ */
+function validateFiles(el) {
+    const {maxCount, maxSize} = el.dataset;
+    const files = el.files;
+    const errorElement = getErrorElement(el);
+
+    if (files.length > maxCount) {
+        errorElement.hidden = false;
+        errorElement.textContent = 'Maximum count of uploaded files: ' + maxCount;
+        return false;
+    }
+
+    for (const file of files) {
+        if (file.size > convertFileSizeToBytes(maxSize)) {
+            errorElement.hidden = false;
+            errorElement.textContent = 'Maximum allowed size of file: ' + maxSize;
+            return false;
+        }
+    }
+
+    errorElement.hidden = true;
+
+    return true;
 }
 
 function sendTicket (e) {
-    e.preventDefault()
+    e.preventDefault();
 
-    if (!validateForm()) {
+    const form = document.querySelector('form');
+    const fileField = document.querySelector('input[type="file"]');
+
+    if (!validateForm() || !validateFiles(fileField)) {
         return;
     }
 
-    console.log(e.currentTarget);
+    const formData = new FormData(form);
+
+    fetch(
+        API_URL,
+        {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+            },
+            body: formData
+        }
+    ).then(response => {
+
+    });
 }
 
-document.querySelector('#submit').addEventListener('click', sendTicket)
+document.querySelector('#submit').addEventListener('click', sendTicket);
+document.querySelector('input[type="file"]')
+    .addEventListener(
+        'change',
+        (ev) => validateFiles(ev.currentTarget)
+    );
