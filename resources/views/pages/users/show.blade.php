3d4bd57bcb19c5f577aeddf281e0995741d6c745
HACKERPRO17 <sartyom532@gmail.com>
2026-04-30 17:32:03 +0300

    feat: Implemented user remove

--- resources/views/pages/users/show.blade.php
+++ resources/views/pages/users/show.blade.php
@@ -4,7 +4,7 @@
     <div class="card mt-3 col-sm-8">
 
         @session('status')
-        <div class="alert alert-success alert-dismissible fade show mt-3" role="alert">
+        <div class="alert alert-{{ session('status-color', 'success') }} alert-dismissible fade show mt-3" role="alert">
             {{ session('status') }}
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
