ca1e162e5cf65406cffc68fbd7e8a93c6a6f66b3
HACKERPRO17 <sartyom532@gmail.com>
2026-04-30 23:04:31 +0300

    test(security): Implemented roles permissions test

---
+++ tests/Feature/Roles/AdminRoleTest.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Tests\Feature\Roles;
+
+use App\Models\Ticket;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class AdminRoleTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private User $user;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->seed();
+
+        $this->user = User::factory()->create()->assignRole('admin');
+    }
+
+    public function testTicketsSee(): void
+    {
+        $response = $this->actingAs($this->user)->get(route('tickets.index'));
+        $response->assertOk();
+    }
+
+    public function testTicketMark(): void
+    {
+        $ticket = Ticket::first();
+        $response = $this->actingAs($this->user)->post(
+            route('tickets.mark', ['id' => $ticket->id])
+        );
+
+        $response->assertRedirect();
+    }
+
+    public function testTicketEdit(): void
+    {
+        $ticket = Ticket::first();
+        $response = $this->actingAs($this->user)
+            ->patch(
+                route(
+                    'tickets.update',
+                    ['ticket' => $ticket],
+                ),
+                [
+                    'status' => fake()->numberBetween(0, 2),
+                ]
+            );
+
+        $response->assertRedirect();
+    }
+
+    public function testTicketDelete(): void
+    {
+        $ticket = Ticket::first();
+        $response = $this->actingAs($this->user)
+            ->delete(route('tickets.destroy', ['ticket' => $ticket]));
+
+        $response->assertRedirect();
+    }
+
+    public function testUsersSee(): void
+    {
+        $response = $this->actingAs($this->user)
+            ->get(route('users.index'));
+
+        $response->assertOk();
+    }
+
+    public function testUserEdit(): void
+    {
+        $roles = ['guest', 'manager', 'admin'];
+        $index = fake()->numberBetween(0, 2);
+
+        $user = User::first();
+        $response = $this->actingAs($this->user)
+            ->patch(
+                route('users.update', ['user' => $user]),
+                [
+                    'role' => $roles[$index],
+                ]
+            );
+
+        $response->assertRedirect();
+    }
+
+    public function testUserRemove(): void
+    {
+        $user = User::whereNot('id', $this->user->id)->first();
+        $response = $this->actingAs($this->user)
+            ->delete(
+                route('users.destroy', ['user' => $user])
+            );
+
+        $response->assertRedirect();
+    }
+}
