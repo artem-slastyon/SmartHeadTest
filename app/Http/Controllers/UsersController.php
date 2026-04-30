1dcc033f0f0efb52a1cbf5e0e736360a9f9fd49e
Artem S. <artyom.slastyon@gmail.com>
2026-04-30 18:22:06 +0300

    feat(users): Implemented user role change

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
@@ -10,6 +11,7 @@ class UsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->middleware('can:edit users')->only('update');
         $this->middleware('can:delete users')->only('destroy');
     }
 
@@ -46,5 +48,18 @@ class UsersController extends Controller
         $user->delete();
 
         return redirect()->route('users.index');
+    }
+
+    public function update(User $user, Request $request)
+    {
+        $data = $request->validate([
+            'role' => 'string|in:guest,manager,admin'
+        ]);
+
+        $user->syncRoles($data['role']);
+
+        return response()
+            ->redirectToRoute('users.show', $user)
+            ->with('status', 'User role was successfully changed');
     }
 }
