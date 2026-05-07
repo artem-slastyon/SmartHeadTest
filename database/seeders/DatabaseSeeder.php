e5cbf264bbeda27116f7d21c8a6e72b282210e0f
HACKERPRO17 <sartyom532@gmail.com>
2026-05-07 12:30:59 +0300

    refactor: Separated test seeders from production seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,23 +16,5 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(RolesAndPermissionsSeeder::class);
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ])->assignRole('admin');
-
-        $roles = ['guest', 'manager', 'admin'];
-
-        User::factory(15)
-            ->create()
-            ->each(function ($user) use ($roles) {
-                $index = fake()->numberBetween(0, 2);
-                $user->assignRole($roles[$index]);
-            });
-
-        $this->call([
-            CustomerSeeder::class
-        ]);
     }
 }
