80c86b59068bca931f1c9b4936f83deb55f30818
HACKERPRO17 <sartyom532@gmail.com>
2026-05-13 14:31:40 +0300

    refactor(users): Refactored check if user can remove/update self

---
+++ app/Policies/UserPolicy.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class UserPolicy
+{
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, User $model): Response
+    {
+        return $user->id !== $model->id ? Response::allow() : Response::deny("You can't update yourself!");
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, User $model): Response
+    {
+        return $user->id !== $model->id ? Response::allow() : Response::deny("You can't remove yourself!");
+    }
+}
