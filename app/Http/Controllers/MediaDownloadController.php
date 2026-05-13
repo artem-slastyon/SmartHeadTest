3d7f5b73c49b4852d4ef1c5eadba5baa60c25034
HACKERPRO17 <sartyom532@gmail.com>
2026-05-13 16:00:53 +0300

    refactor(media): Refactored media download controller

--- app/Http/Controllers/MediaDownloadController.php
+++ app/Http/Controllers/MediaDownloadController.php
@@ -2,14 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class MediaDownloadController extends Controller
 {
-    public function index(int $id)
+    public function index(Request $request, int $id)
     {
         $media = Media::find($id);
 
-        return response()->download($media->getPath(), $media->file_name);
+        return $media->toResponse($request);
     }
 }
